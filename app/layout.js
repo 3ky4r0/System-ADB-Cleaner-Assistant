8727ff4cfdf962308a624c24d39935b800d80908
duyxyz <duyxyz.git@gmail.com>
2026-09-11 20:26:07 +0700

    d

--- app/layout.js
+++ app/layout.js
@@ -9,7 +9,7 @@ const dancingScript = Dancing_Script({
 });
 
 export const metadata = {
-  title: "SACA - System ADB Cleaner Assistant",
+  title: "System ADB Cleaner Assistant",
   description: "Interactive Web app to list and uninstall Android packages via ADB",
 };
 
